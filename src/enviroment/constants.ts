f4768bfb3e2d4f6dffa45d49c4b471bafbf6dac3
Andrés Pantoja <55747009+andresPantoja99@users.noreply.github.com>
2022-12-19 12:40:53 -0500

    Update constants.ts

--- src/enviroment/constants.ts
+++ src/enviroment/constants.ts
@@ -3,10 +3,10 @@ import { ENTITIES } from './entities';
 export const CONSTANTS = {
   DB: {
     host: 'localhost',
-    port: 49154,
-    username: 'root',
-    password: 'root',
-    database: 'postgres',
+    port: ,
+    username: '',
+    password: '',
+    database: '',
     entities: [...ENTITIES],
     autoLoadEntitites: true,
     synchronize: true,
